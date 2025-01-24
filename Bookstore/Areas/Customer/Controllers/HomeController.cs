07d7207826dfab470b1c11fe930eb5e1b687221a
Filip Jovanovic <speklja996@gmail.com>
2025-01-24 20:25:17 +0100

    Added Home and Details Page

--- Bookstore/Areas/Customer/Controllers/HomeController.cs
+++ Bookstore/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Bookstore.DataAccess.Repository.IRepository;
 using Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,26 @@ namespace Bookstore.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _uow;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork uow)
         {
             _logger = logger;
+            _uow = uow;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _uow.ProductRepository.GetAll(includeProperties: "Category");
+
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product product = _uow.ProductRepository.Get(p => p.Id == id, includeProperties: "Category");
+
+            return View(product);
         }
 
         public IActionResult Privacy()
